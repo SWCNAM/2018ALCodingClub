2af312f9f0dc0511e6b1d14df08c4dc87cd2cc77
SWCNAM <41236868+SWCNAM@users.noreply.github.com>
2018-09-29 12:52:47 -0500

    animations and keybindings

--- Lesson 8/index.js
+++ Lesson 8/index.js
@@ -1,5 +1,6 @@
 var stage, w, h, loader;
-var player;
+var playerSprite;
+var keyHandler = false;
 
 init();
 
@@ -11,7 +12,7 @@ function init() {
     h = stage.canvas.height;
 
     manifest = [
-        {src: "../Assets/kenney_platformercharacters/PNG/Player/player.json", id: "player",type:"spritesheet"},
+        { src: "../Assets/kenney_platformercharacters/PNG/Player/player.json", id: "player", type: "spritesheet" },
     ];
 
     loader = new createjs.LoadQueue(false);
@@ -20,18 +21,63 @@ function init() {
 }
 
 function tick(event) {
+    playerSprite = stage.getChildByName("player");
+    if (keyHandler == "ArrowRight") {
+        createjs.Tween.get(playerSprite)
+        .to({ x: playerSprite.x + 20, scaleX: 1 }, 200)
+    }
+    else if (keyHandler == "ArrowLeft") {
+        createjs.Tween.get(playerSprite)
+        .to({ x: playerSprite.x - 20, scaleX: -1 }, 200)
+    } else {
+        
+    }
+
     stage.update(event);
 }
 
-function handleComplete(){
-    player = loader.getResult("player");
+function handleComplete() {
+    var player = loader.getResult("player");
     player.getAnimation("player_idle").next = "player_cheer";
     player.getAnimation("player_cheer").next = "player_stand";
     var playerSprite = new createjs.Sprite(player, "player_idle");
     playerSprite.y = 150;
+    playerSprite.x = 80;
+    playerSprite.name = "player";
 
     stage.addChild(playerSprite);
+    this.document.onkeydown = handleKeyDown;
+    this.document.onkeyup = handleKeyUp;
 
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", tick);
+
+}
+
+function handleKeyDown(event) {
+    if (keyHandler == null) {
+        playerSprite = stage.getChildByName("player");
+        playerSprite.gotoAndPlay("player_walk");
+        switch (event.code) {
+            case "ArrowRight":
+                keyHandler = "ArrowRight";
+                break;
+            case "ArrowLeft":
+                keyHandler = "ArrowLeft";
+                break;
+            case "Space":
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+function handleKeyUp(event) {
+    keyHandler = null;
+    if (event.code != "Space") {
+        playerSprite = stage.getChildByName("player");
+        playerSprite.stop();
+        playerSprite.gotoAndPlay("player_stand");
+    }
 }
